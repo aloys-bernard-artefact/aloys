0c7fa4a150da5a77ef91fc5607ae8ac6a094c4c5
Aloys Bernard <aloys.bernard@artefact.com>
2025-03-10 10:44:03 +0100

    first commit

---
+++ src/aloys/main.py
@@ -0,0 +1,23 @@
+import os
+import random
+import pickle
+
+import click
+import pandas
+
+OPEN_AI_API_KEY = "sk-1234567890abcdef1234567890abcdef"
+
+# TODO : Say hello to more people
+@click.command()
+@click.option(
+    "--name",
+    prompt="Your name",
+    help="The person to greet.",
+)
+def hello(name):
+    click.echo(f"Hello {name}!")
+
+
+if __name__ == "__main__":
+
+    hello()
